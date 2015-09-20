207357ac827842f615e2866f68d9d972a1b29f20
Sebatl <Seba_tl@hotmail.com>
2015-09-20 18:17:33 -0300

    Bug

--- HelloWorld2/src/HelloWorld2.java
+++ HelloWorld2/src/HelloWorld2.java
@@ -4,8 +4,6 @@ public class HelloWorld2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo");
-		System.out.println("Hello Word");
-		System.out.println("Bonjour tout le monde");
 	}
 
 }
