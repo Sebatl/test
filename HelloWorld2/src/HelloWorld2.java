8fc1ef659d7d38d4137f50d20a69da40eabbddd4
Sebatl <Seba_tl@hotmail.com>
2015-09-20 13:29:44 -0300

    Update Out.println

--- HelloWorld2/src/HelloWorld2.java
+++ HelloWorld2/src/HelloWorld2.java
@@ -4,6 +4,7 @@ public class HelloWorld2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo");
+		System.out.println("Hello Word");
 	}
 
 }
