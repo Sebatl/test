d2b6ed693527d83759d33086619999c78526f91c
Sebatl <Seba_tl@hotmail.com>
2015-09-22 08:57:09 -0300

    TranslatorTest v2.1

--- HelloWorld2/src/testing/TranslatorTest.java
+++ HelloWorld2/src/testing/TranslatorTest.java
@@ -35,7 +35,7 @@ private Translator translator;
 	 */
 	@Test
 	public void testPrintHelloWorld() {
-		assertEquals("No coincide con Hola mundo","Hola mundo", translator.printHelloWorld("ESP"));
+		assertEquals("No coincide con Hola mundo","Hola mundo", translator.printHelloWorld("SPA"));
 		assertEquals("No coincide con Hello world","Hello World", translator.printHelloWorld("ENG"));
 	}
 
